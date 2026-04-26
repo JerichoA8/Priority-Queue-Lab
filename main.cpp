537de6d751590b62972faac01111545a3cdc087a
JerichoA8 <ss85143@mail.harpercollege.edu>
2026-04-26 16:33:56 -0500

    Completed Lab
    Completed Priority Queue Lab

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+template <typename T>
+class PriorityQueue {
+private:
+    vector<T> data;
+    function<double(T)> priority;
+
+public:
+    PriorityQueue(function<double(T)> priority_func) {
+        priority = priority_func;
+    }
+
+    void push(const T& val) {
+        data.push_back(val);
+        sort(data.begin(), data.end(), [this](const T& a, const T& b) {
+            return priority(a) > priority(b);
+        });
+    }
+
+    T pop() {
+        if (is_empty()) {
+            cout << "Error: PriorityQueue is empty!" << endl;
+            exit(1);
+        }
+        T front = data.front();
+        data.erase(data.begin());
+        return front;
+    }
+
+    bool is_empty() const { return data.empty(); }
+};
+
+int main() {
+    PriorityQueue<string> pq([](string s){return (double)s.length();});
+
+    pq.push("hello");
+    pq.push("philippines");
+    pq.push("ball");
+    pq.push("jericho");
+
+    while (!pq.is_empty()) {
+        cout << pq.pop() << endl;
+    }
+
+    return 0;
+}
